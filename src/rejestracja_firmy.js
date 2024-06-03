7d4be905bc3d61b6d5f2f4a389f5d631c2a9290a
Michał Zielonka <zielony413@gmail.com>
2024-06-04 00:31:24 +0200

    Logo, nazwa, walidacja rejestracji firmy

--- src/rejestracja_firmy.js
+++ src/rejestracja_firmy.js
@@ -153,12 +153,22 @@ const handleSubmit = async () => {
       const data = await response.json();
       if (response.ok) {
           console.log('Form submitted successfully:', data);
+<<<<<<< Updated upstream
+=======
+          alert('Dane poprawne. Kontynuuj');
+          // Reset form fields if needed
+>>>>>>> Stashed changes
           
       } else {
+<<<<<<< Updated upstream
 
         if (response.status === 400) {
           alert(response.data.message);
           console.error('Error submitting form:', data.error);}
+=======
+          console.error('Error submitting form:', data.error);
+          alert('Niepoprawne dane. Sprawdź wypełnione pola.');
+>>>>>>> Stashed changes
       }
   } catch (error) {
       console.error('An error occurred while submitting the form:', error);
@@ -166,7 +176,11 @@ const handleSubmit = async () => {
 };
 
 const handleFinish = () => {
+<<<<<<< Updated upstream
   navigate('/');
+=======
+  navigate('/logowanie');
+>>>>>>> Stashed changes
 };
 
   
@@ -318,7 +332,7 @@ const handleFinish = () => {
       };
 
       try {
-        const response = await fetch('/api/add_service', {
+        const response = await fetch('/api/strona_rejestracji_firmy/usługa', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
